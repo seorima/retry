cd5a715dfe4f153df2aa4373db69fd430fb8e495
[seorima@naver.com] <[seorima@naver.com]>
2021-06-24 21:21:00 +0900

    color

--- app/src/main/java/com/cookandroid/aa/LoginActivity.java
+++ app/src/main/java/com/cookandroid/aa/LoginActivity.java
@@ -34,6 +34,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+        Button btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+
+
+        });
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("aa");
 
@@ -73,16 +84,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
-        Button btn_register = findViewById(R.id.btn_register);
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
 
-
-        });
 
 
 
