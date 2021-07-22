01c1cf5776bacedb234fb3de4d9bdbae4e0a1db1
yerim8373 <yerim8373@naver.com>
2021-07-22 21:00:02 +0900

    up / down /

--- app/src/main/java/com/cookandroid/aa/ControlActivity.java
+++ app/src/main/java/com/cookandroid/aa/ControlActivity.java
@@ -8,7 +8,13 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ControlActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     private ImageButton ibtn_blind_up, ibtn_blind_stop, ibtn_blind_down;
     private Button btn_light_control, btn_alarm, btn_mode;
@@ -25,11 +31,28 @@ public class ControlActivity extends AppCompatActivity {
         btn_alarm = findViewById(R.id.btn_alarm);
         btn_mode = findViewById(R.id.btn_mode);
 
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("aa");
+
 
         ibtn_blind_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                myRef.child("Control").child("blind_control").setValue("up");
+            }
+        });
 
+        ibtn_blind_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.child("Control").child("blind_control").setValue("stop");
+            }
+        });
+
+        ibtn_blind_down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.child("Control").child("blind_control").setValue("down");
             }
         });
 
